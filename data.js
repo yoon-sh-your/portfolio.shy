cbcd5cb1948f74e27ac884a6ee24ff3efb951832
yoon-sh-your <yoon.sh.yours@gmail.com>
2025-04-13 19:57:05 +0900

    add interactive section,scroll-pt section

---
+++ data.js
@@ -0,0 +1,86 @@
+export const mapClasses = [
+  "variant-1",
+  "variant-2",
+  "variant-3",
+  "variant-1",
+  "variant-2",
+  "variant-3",
+  "variant-1",
+  "variant-2",
+  "variant-3",
+  "variant-1",
+];
+
+export const previews = [
+  {
+    img: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Ra",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1669324358569-45d5d4696ea7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "miu",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1737757512943-0a23ab651728?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "do",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1737757512928-9011cde830f8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "re",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1737757512943-0a23ab651728?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "me",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1737058528299-3e1c9a5bb913?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "pa",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1737050752195-50c39f83420e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "sol",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1736536058883-df19af39b91b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "si",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=1910&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "do",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1546552916-985b466ffbec?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "sira",
+    tags: "Futuristic Fashion, Minimal Design",
+    description:
+      "Exploring the intersection of minimalism and future fashion trends in web design.",
+  },
+
+];
